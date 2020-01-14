aa7c7a072eb349e39f92b2782a1b6cc7146b1121
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-01-14 13:03:34 +1100

    finished creating items with mutations

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,18 @@
-const mutations = {};
+const Mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: check if they are logged in
 
-module.exports = mutations;
+    const item = await ctx.db.mutation.createItem({
+      data: {
+        ...args
+      }
+    }, 
+    info);
+
+    console.log(item);
+
+    return item;
+  },
+};
+
+module.exports = Mutations;
--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,8 +1,9 @@
+import CreateItem from '../components/CreateItem';
 import Link from 'next/link';
 
 const Sell = props => (
   <div>
-    <p>Sell!</p>
+    <CreateItem />
   </div>
 );
 
