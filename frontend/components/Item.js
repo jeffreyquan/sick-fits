2a3a3a7978a8644ec82b2db82bba7717fd18af6b
Jeffrey Quan <jeffrey.quan@hotmail.com>
2020-01-20 18:20:20 +1100

    added delete item component

--- frontend/components/Item.js
+++ frontend/components/Item.js
@@ -6,7 +6,7 @@ import ItemStyles from './styles/ItemStyles';
 import PriceTag from './styles/PriceTag';
 import formatMoney from '../lib/formatMoney';
 import { format } from 'date-fns';
-
+import DeleteItem from './DeleteItem';
 
 export default class Item extends Component {
   static propTypes = {
@@ -36,7 +36,7 @@ export default class Item extends Component {
             <a>Edit</a>
           </Link>
           <button>Add To Cart</button>
-          <button>Delete</button>
+          <DeleteItem id={ item.id }>Delete This Item</DeleteItem>
         </div>
       </ItemStyles>
     );
