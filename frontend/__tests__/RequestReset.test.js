6649a4ce3cafbccf756844dbea036da48ffa1892
Jeffrey <jeffrey.quan@hotmail.com>
2022-03-25 00:23:17 +1100

    test: add test for request reset

---
+++ frontend/__tests__/RequestReset.test.js
@@ -0,0 +1,49 @@
+import { render, screen } from "@testing-library/react";
+import { MockedProvider } from "@apollo/client/testing";
+import userEvent from "@testing-library/user-event";
+import RequestReset, {
+  REQUEST_RESET_MUTATION,
+} from "../components/RequestReset";
+
+const email = "wesbos@gmail.com";
+
+const mocks = [
+  {
+    request: {
+      query: REQUEST_RESET_MUTATION,
+      variables: { email },
+    },
+    result: {
+      data: {
+        sendUserPasswordResetLink: null,
+      },
+    },
+  },
+];
+
+describe("<RequestReset/>", () => {
+  it("renders and matches snapshot", () => {
+    const { container } = render(
+      <MockedProvider>
+        <RequestReset />
+      </MockedProvider>
+    );
+
+    expect(container).toMatchSnapshot();
+  });
+
+  it("calls the mutation when submitted", async () => {
+    const { container } = render(
+      <MockedProvider mocks={mocks}>
+        <RequestReset />
+      </MockedProvider>
+    );
+
+    // type into the email box
+    userEvent.type(screen.getByPlaceholderText(/email/i), email);
+    // click submit
+    userEvent.click(screen.getByText(/Request Reset/i));
+    const success = await screen.findByText(/Success/i);
+    expect(success).toBeInTheDocument();
+  });
+});
--- frontend/components/RequestReset.js
+++ frontend/components/RequestReset.js
@@ -4,7 +4,7 @@ import { useMutation } from "@apollo/client";
 import gql from "graphql-tag";
 import Error from "./ErrorMessage";
 
-const REQUEST_RESET_MUTATION = gql`
+export const REQUEST_RESET_MUTATION = gql`
   mutation REQUEST_RESET_MUTATION($email: String!) {
     sendUserPasswordResetLink(email: $email) {
       code
